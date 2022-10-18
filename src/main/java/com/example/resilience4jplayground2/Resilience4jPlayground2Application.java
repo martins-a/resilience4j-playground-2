f297497e376e0d398e2e6d145b9f3dab4c1f3602
at-martins <at-martins@outlook.com>
2022-10-17 23:13:43 -0300

    Init

---
+++ src/main/java/com/example/resilience4jplayground2/Resilience4jPlayground2Application.java
@@ -0,0 +1,13 @@
+package com.example.resilience4jplayground2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Resilience4jPlayground2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Resilience4jPlayground2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/resilience4jplayground2/Resilience4jPlayground2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.resilience4jplayground2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Resilience4jPlayground2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
